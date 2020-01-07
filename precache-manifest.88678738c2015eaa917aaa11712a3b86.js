47490be821fff64b7667e2f59f70d93f4d4b91b4
joqtan <joqtanmarmol@gmail.com>
2020-01-06 20:29:15 -0600

    Updates

---
+++ precache-manifest.88678738c2015eaa917aaa11712a3b86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c9ddf80d8decaa8b22c37e8edb1ce3",
+    "url": "/leaguedex/index.html"
+  },
+  {
+    "revision": "ba4535c15cc05cb66734",
+    "url": "/leaguedex/static/css/2.75763e73.chunk.css"
+  },
+  {
+    "revision": "ba4535c15cc05cb66734",
+    "url": "/leaguedex/static/js/2.3397c901.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/leaguedex/static/js/2.3397c901.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38f082dfe7b7b30ec02f",
+    "url": "/leaguedex/static/js/main.202bd52f.chunk.js"
+  },
+  {
+    "revision": "1f73ac39226385964d74",
+    "url": "/leaguedex/static/js/runtime-main.a125d6c2.js"
+  }
+]);
